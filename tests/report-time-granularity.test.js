fc7af9f65ce4a5c3df20c4046239ec69f2700a9c
叶研 <fangruiyan.fry@alibaba-inc.com>
2026-09-11 17:45:48 +0800

    chore:收敛报表测试预期警告输出 to #86524791

--- tests/report-time-granularity.test.js
+++ tests/report-time-granularity.test.js
@@ -1,5 +1,12 @@
 'use strict';
 
+jest.mock('../lib/core/chalk', () => ({
+  ...jest.requireActual('../lib/core/chalk'),
+  warn: jest.fn(),
+}));
+
+const { warn } = require('../lib/core/chalk');
+
 const { buildDataSetModelMap } = require('../lib/report/data-model');
 const { validateChartConfig } = require('../lib/report/chart-builder');
 
@@ -118,8 +125,13 @@ describe('report timeGranularityType', () => {
 
   test.each(['QUARTER', 'WEEK', 'DECADE'])('%s 不能通过配置校验，也不能被构建器静默回落', (granularity) => {
     const chart = chartCases[0].chart(granularity);
+    warn.mockClear();
 
     expect(validateChartConfig(chart, 0)).toBe(false);
+    expect(warn).toHaveBeenCalledTimes(1);
+    expect(warn).toHaveBeenCalledWith(expect.stringContaining(
+      'charts[0].fields[0].timeGranularityType: 不支持的 timeGranularityType'
+    ));
     expect(() => buildDataSetModelMap(chart, 'corp-1')).toThrow(
       new RegExp(`timeGranularityType.*${granularity}`)
     );
